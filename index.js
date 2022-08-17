4951c3cec4acef73abe368909b8ee70b699c1d92
achilles schmelzer <achilles@daopanel.com>
2022-08-17 16:54:08 -0700

    Test a couple different methods

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import { Client } from '@xmtp/xmtp-js';
 import { Wallet } from 'ethers';
-const wallet = new Wallet(process.env.MY_PRIVATE_KEY)
-const client = await Client.create(wallet);
-const messages = await client.listConversationMessages(wallet.address, { pageSize: 10 });
-console.log(messages.length); // More than 10
+
+const main = async () => {
+  const wallet = new Wallet(process.env.MY_PRIVATE_KEY)
+  const client = await Client.create(wallet);
+  const viaClient = await client.listConversationMessages(wallet.address, { pageSize: 10 });
+  const conversations = await client.conversations.list();
+  const withMyself = conversations.find(conversation => conversation.peerAddress === wallet.address);
+  const viaConversation = await withMyself.messages({ pageSize: 10 });
+  console.log('Number in viaConversation:', viaConversation.length)
+  console.log('Number in viaClient:', viaClient.length)
+}
 
 
+main();
 
